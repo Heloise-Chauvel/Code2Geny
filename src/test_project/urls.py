5b58d7e79386fe4af535014003458fb74c93242e
Héloïse Chauvel <chauvel.h@gmail.com>
2019-01-20 22:42:03 -0400

    Fixed authentication (Django last update) + reorganized urlpatterns

--- src/test_project/urls.py
+++ src/test_project/urls.py
@@ -13,49 +13,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from . import view
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    #login
+    path('', auth_views.LoginView.as_view(template_name = 'registration/login.html'), name='login'),
+    url(r'login/$', auth_views.LoginView.as_view(template_name = 'registration/login.html'), name='login'),
+    #url(r'^logout/$', auth_views.LoginView.as_view(template_name = 'registration/logout.html', name='logout'),
+    #index
+    path('index/', view.index),
+    #admin
+    path('admin/', admin.site.urls),
+    #error/
+    url(r'^error/$',view.error,name='invoice'),
+
+    #cours
+    path('cours', view.cours),
+    #invoice
+    url(r'^invoice/$', view.invoiceView, name='invoice'),
+
     #/classe/[xxx]/
-    url(r'^classe/([0-9]+)/$',view.detailsClasse,name='detailsClasse'),
+    url(r'^classe/([0-9]+)/$',view.detailsClasse, name='detailsClasse'),
     #create/[xxx]/classe/[xxx]
     url(r'^create/([0-9]+)/classe/([0-9]+)/$',view.detailsClasse2,name='detailsClasse'),
     #/devoir/[xxx]/
     url(r'^devoir/([0-9]+)/$',view.detailsDevoir,name='detailsDevoir'),
     #/devoir/creerDevoir/([0-9]+)
     url(r'^devoir/creerDevoir/([0-9]+)/$',view.remplirNouveauDevoir,name='creerDevoir'),
-    #login
-    path('', auth_views.login, name='login'),
     # #form devoir prof   create/[xxx]
     url(r'^create/([0-9]+)/$', view.devoir_create,name="createDevoir"),
     #form rendre Devoir  create/[xxx]
     url(r'^rendre/([0-9]+)/$', view.devoir_rendre,name="rendreDevoir"),
     #Recuperer les notes d'un devoir
     url(r'^recupNote/([0-9]+)/$', view.recupNote,name="recupNote"),
-    #index
-    path('index/', view.index),
-    #admin
-    path('admin/', admin.site.urls),
-    #invoice
-    url(r'^invoice/$', view.invoiceView, name='invoice'),
-path('cours', view.cours),
-    #login?
-    url(r'login/$', auth_views.LoginView.as_view(template_name='templates/registration/login.html'), name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-    #invoice/
-    url(r'^invoice/$',view.invoiceView,name='invoice'),
-    #error/
-     url(r'^error/$',view.error,name='invoice'),
-
-
-
 ]
 
 if settings.DEBUG:
